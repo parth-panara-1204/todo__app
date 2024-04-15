c053d3cc6e4d60f154bd576c67d61735bdc32eba
parth-panara-1204 <parthpanara24@gmail.com>
2024-04-15 12:27:54 +0530

    logs added

--- main.cpp
+++ main.cpp
@@ -1,112 +1,143 @@
 #include<iostream>
 #include<fstream>
+#include<chrono>
+#include<ctime>
 
 using namespace std;
 
-void add() {
-    // adds a task to task.txt file
-    string task;
-    cout<<"enter task: ";
-    getline(cin, task);
+class task {
+    string t;
 
-    ofstream out("task.txt", ios::app);
-    out<<task<<endl;
-    out.close();
-}
-
-void list(string path) {
-    // lists all the tasks in the task.txt file
-    int i=1;
-    string res;
-
-    ifstream in(path);
-    getline(in, res);
-
-    while (res != "") {
-        cout << i <<". "<< res << endl;
-        getline(in, res);
-        i++;
-    }
-
-    in.close();
-}
-
-void delete_task(int d) {
-    // delets a task in the task.txt file, given line number
-    int i = 1;
-    string s;
-
-    ifstream in("task.txt");
-
-    ofstream out("temp.txt");
-
-    if (d == 1) {
-        getline(in, s);
-        i += 1;
-    }
-
-    while (getline(in, s)) {
-        if (i == d) {
-            i += 1;
-            continue;
+    public:
+        void get_task() {
+            cout << "Enter the task: ";
+            getline(cin, t);
         }
-        out<<s<<endl;
-        i += 1;
-    }
 
-    in.close();
-    out.close();
+        void add() {
+            // adds a task to task.txt file
+            get_task();
 
-    remove("task.txt");
-    rename("temp.txt", "task.txt");
-}
+            ofstream out("task.txt", ios::app);
+            out << t << endl;
+            out.close();
 
-void done(int dn) {
-    // marks a task done in done.txt file
-    int i=1;
-    string res;
-
-    ifstream in1("task.txt");
-    getline(in1, res);
-
-    ofstream out1("done.txt");
-
-    while (res != "") {
-        if (i == dn) {
-            out1<<res<<endl;
-            out1.close();
-            in1.close();
-            delete_task(i);
+            out.open("logs.txt",ios::app);
+            out << log() << "   task added: " << t << endl;
+            out.close();
         }
-        getline(in1, res);
-        i++;
-    }
 
+        void list(string path) {
+            // lists all the tasks in the task.txt file
+            int i=1;
+            string res;
 
-}
+            ifstream in(path);
+            getline(in, res);
+
+            while (res != "") {
+                cout << i <<". "<< res << endl;
+                getline(in, res);
+                i++;
+            }
+            in.close();
+        }
+
+        void delete_task(int d) {
+            // delets a task in the task.txt file, given line number
+            int i = 1;
+            string s;
+
+            ifstream in("task.txt");
+
+            ofstream out("temp.txt");
+
+            // if (d == 1) {
+            //     getline(in, s);
+            //     i += 1;
+            //     ofstream o("logs.txt", ios::app);
+            //     o << log() << "   task deleted: " << s << endl;
+            //     o.close();
+            // }
+
+            while (getline(in, s)) {
+                if (i == d) {
+                    i += 1;
+                    
+                    ofstream o("logs.txt", ios::app);
+                    o << log() << "   task deleted: " << s << endl;
+                    o.close();
+                    continue;
+                }
+                out<<s<<endl;
+                i += 1;
+            }
+
+            in.close();
+            out.close();
+
+            remove("task.txt");
+            rename("temp.txt", "task.txt");
+        }
+
+        void done(int dn) {
+            // marks a task done in done.txt file
+            int i=1;
+            string res;
+
+            ifstream in1("task.txt");
+            getline(in1, res);
+
+            ofstream out1("done.txt", ios::app);
+
+            while (res != "") {
+                if (i == dn) {
+                    out1<<res<<endl;
+                    out1.close();
+                    
+                    out1.open("logs.txt", ios::app);
+                    out1 << log() << "  task complete!: " << res << endl;
+                    
+                    out1.close();
+                    in1.close();
+                    delete_task(i);
+                }
+                getline(in1, res);
+                i++;
+            }
+        }
+
+        string log() {
+            auto now = std::chrono::system_clock::now();
+            time_t now_t = std::chrono::system_clock::to_time_t(now);
+            return ctime(&now_t);
+        }
+};
 
 int main() {
-    add();
+    task T;
+
+    T.add();
 
     cout<<endl<<"-------------------------------------------------------------------------------"<<endl;
-    list("task.txt");
+    T.list("task.txt");
     cout<<endl<<"-------------------------------------------------------------------------------"<<endl;
 
     cout<<endl<<"enter the number of line you wanna delete: ";
     int m;
     cin>>m;
-    delete_task(m);
+    T.delete_task(m);
 
     cout<<endl<<"-------------------------------------------------------------------------------"<<endl;
-    list("task.txt");
+    T.list("task.txt");
     cout<<endl<<"-------------------------------------------------------------------------------"<<endl;
 
     cout<<endl<<"enter the no of task you want to mark done: ";
     cin>>m;
-    done(m);
+    T.done(m);
 
     cout<<endl<<"-------------------------------------------------------------------------------"<<endl;
-    list("task.txt");
+    T.list("task.txt");
     cout<<endl<<"-------------------------------------------------------------------------------"<<endl;
 
     return 0;
